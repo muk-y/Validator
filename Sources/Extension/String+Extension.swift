873bb3d3e69ca99040550f7dc567dde4ac29d7cd
Mukesh Shakya <muky@Mukeshs-MacBook-Pro.local>
2022-01-04 12:57:07 +0545

    Localization variable added in string extension

--- Sources/Extension/String+Extension.swift
+++ Sources/Extension/String+Extension.swift
@@ -19,4 +19,10 @@ extension String {
         return try validator.validated(self)
     }
     
+    var localized: String {
+        guard let path = Bundle.module.path(forResource: UserDefaults.standard.string(forKey: "LCLCurrentLanguageKey") ?? Locale.current.languageCode, ofType: "lproj") else { return ""}
+        guard let bundle = Bundle(path: path) else { return "" }
+        return NSLocalizedString(self, bundle: bundle, comment: "")
+    }
+    
 }
